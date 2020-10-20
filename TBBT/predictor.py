95cc73366055253bf60bf5c4cc1be4c59d7ffd06
diego <diegoandre.gomez@gmail.com>
2020-10-20 12:39:49 +0200

    try to just draw people

--- TBBT/predictor.py
+++ TBBT/predictor.py
@@ -88,12 +88,12 @@ class VisualizationDemo(object):
 
         def process_predictions(frame, predictions):
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if "panoptic_seg" in predictions:
-                panoptic_seg, segments_info = predictions["panoptic_seg"]
-                vis_frame = video_visualizer.draw_panoptic_seg_predictions(
-                    frame, panoptic_seg.to(self.cpu_device), segments_info
-                )
-            elif "instances" in predictions:
+            # if "panoptic_seg" in predictions:
+            #     panoptic_seg, segments_info = predictions["panoptic_seg"]
+            #     vis_frame = video_visualizer.draw_panoptic_seg_predictions(
+            #         frame, panoptic_seg.to(self.cpu_device), segments_info
+            #     )
+            if "instances" in predictions:
                 predictions = predictions["instances"].to(self.cpu_device)
                 ########################################################################################################################################
                 pred_c = predictions.pred_classes
@@ -106,10 +106,10 @@ class VisualizationDemo(object):
                 vis_frame = video_visualizer.draw_instance_predictions(frame, pred_people)
                 ########################################################################################################################################
                 #vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
-            elif "sem_seg" in predictions:
-                vis_frame = video_visualizer.draw_sem_seg(
-                    frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
-                )
+            # elif "sem_seg" in predictions:
+            #     vis_frame = video_visualizer.draw_sem_seg(
+            #         frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
+            #     )
 
             # Converts Matplotlib RGB format to OpenCV BGR format
             vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
