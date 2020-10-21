ca81922e439b48f13b988dda16f9919e1bc15b14
diego <diegoandre.gomez@gmail.com>
2020-10-21 13:36:41 +0200

    changes

--- TBBT/predictor.py
+++ TBBT/predictor.py
@@ -151,7 +151,11 @@ class VisualizationDemo(object):
                         # predictions.set("pred_boxes",d2.Boxes(pred_people_box))
                         # predictions.pred_classes = pred_people_c
 
-                        new_pred = d2.Instances(predictions.image_size,[pred_people_box,scores,pred_people_classes,pred_people_masks])
+                        new_pred = d2.Instances(predictions.image_size)
+                        new_pred.pred_classes = pred_people_classes
+                        new_pred.pred_boxes = pred_people_box
+                        new_pred.scores = scores
+                        new_pred.pred_masks = pred_people_masks
                         vis_frame = video_visualizer.draw_instance_predictions(frame, new_pred)
                         ########################################################################################################################################
                         #vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
